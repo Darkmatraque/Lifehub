f6751244020d99d35070079869f7f43e9ff1f426
Darkmatraque <kevin2007baumann@gmail.com>
2026-07-13 21:21:25 +0200

    Update Notes.tsx

---
+++ lifehub/src/pages/Notes.tsx
@@ -0,0 +1,25 @@
+import PageContainer from "../components/layout/PageContainer";
+
+export default function Notes() {
+
+    return (
+
+        <PageContainer>
+
+            <h1>
+
+                Notes
+
+            </h1>
+
+            <p>
+
+                Toutes tes notes seront affichées ici.
+
+            </p>
+
+        </PageContainer>
+
+    );
+
+}
