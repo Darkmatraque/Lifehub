3ec8c6c063ee1429b8514974355b6bbfb733389d
Darkmatraque <kevin2007baumann@gmail.com>
2026-07-13 21:39:43 +0200

    Update Calendar.tsx

---
+++ lifehub/src/pages/Calendar.tsx
@@ -0,0 +1,21 @@
+import PageContainer from "../components/layout/PageContainer";
+
+export default function Calendar(){
+
+    return(
+
+        <PageContainer>
+
+            <h1>📅 Calendrier</h1>
+
+            <p>
+
+                Cette partie sera développée prochainement.
+
+            </p>
+
+        </PageContainer>
+
+    )
+
+}
