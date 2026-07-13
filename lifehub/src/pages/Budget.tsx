27fe00147d1fb19e7eeb356734cd7e567e0f087e
Darkmatraque <kevin2007baumann@gmail.com>
2026-07-13 21:39:51 +0200

    Update Budget.tsx

---
+++ lifehub/src/pages/Budget.tsx
@@ -0,0 +1,21 @@
+import PageContainer from "../components/layout/PageContainer";
+
+export default function Budget(){
+
+    return(
+
+        <PageContainer>
+
+            <h1>💰 Budget</h1>
+
+            <p>
+
+                Gère ici toutes tes dépenses.
+
+            </p>
+
+        </PageContainer>
+
+    )
+
+}
