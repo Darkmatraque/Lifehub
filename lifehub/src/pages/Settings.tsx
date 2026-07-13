8a8a64f72fa872db7a36437bf1ebd1c45cdc3319
Darkmatraque <kevin2007baumann@gmail.com>
2026-07-13 21:40:15 +0200

    Update Settings.tsx

---
+++ lifehub/src/pages/Settings.tsx
@@ -0,0 +1,21 @@
+import PageContainer from "../components/layout/PageContainer";
+
+export default function Settings(){
+
+    return(
+
+        <PageContainer>
+
+            <h1>⚙️ Paramètres</h1>
+
+            <p>
+
+                Configure LifeHub.
+
+            </p>
+
+        </PageContainer>
+
+    )
+
+}
