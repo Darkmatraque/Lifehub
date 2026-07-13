02c4cfaaddaf3d530c2f5cda72ac67513c538144
Darkmatraque <kevin2007baumann@gmail.com>
2026-07-13 21:21:16 +0200

    Update Todo.tsx

---
+++ lifehub/src/pages/Todo.tsx
@@ -0,0 +1,25 @@
+import PageContainer from "../components/layout/PageContainer";
+
+export default function Todo() {
+
+    return (
+
+        <PageContainer>
+
+            <h1>
+
+                To-do
+
+            </h1>
+
+            <p>
+
+                Cette page accueillera bientôt ton gestionnaire de tâches.
+
+            </p>
+
+        </PageContainer>
+
+    );
+
+}
