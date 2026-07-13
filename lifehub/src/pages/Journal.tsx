efd393ab578038795b5d633f5dcb345cf6bc1e9b
Darkmatraque <kevin2007baumann@gmail.com>
2026-07-13 21:40:09 +0200

    Update Journal.tsx

---
+++ lifehub/src/pages/Journal.tsx
@@ -0,0 +1,21 @@
+import PageContainer from "../components/layout/PageContainer";
+
+export default function Journal(){
+
+    return(
+
+        <PageContainer>
+
+            <h1>📖 Journal</h1>
+
+            <p>
+
+                Ton journal personnel.
+
+            </p>
+
+        </PageContainer>
+
+    )
+
+}
