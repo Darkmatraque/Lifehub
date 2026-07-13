5713609fe3329c7839bcc808624dc392d1a3c973
Darkmatraque <kevin2007baumann@gmail.com>
2026-07-13 21:39:28 +0200

    Update App.tsx

--- lifehub/src/App.tsx
+++ lifehub/src/App.tsx
@@ -1,14 +1,60 @@
 import "./styles/global.css";
 import "./styles/layout.css";
-import "./styles/components.css";
 import "./styles/pages.css";
+import "./styles/components.css";
 
 import Sidebar from "./components/layout/Sidebar";
 import Header from "./components/layout/Header";
 
 import Dashboard from "./pages/Dashboard";
+import Todo from "./pages/Todo";
+import Notes from "./pages/Notes";
+import Calendar from "./pages/Calendar";
+import Budget from "./pages/Budget";
+import Goals from "./pages/Goals";
+import Journal from "./pages/Journal";
+import Settings from "./pages/Settings";
+
+import { useAppStore } from "./store/appStore";
 
 export default function App() {
+
+    const page = useAppStore(state => state.currentPage);
+
+    function renderPage() {
+
+        switch(page){
+
+            case "dashboard":
+                return <Dashboard />;
+
+            case "todo":
+                return <Todo />;
+
+            case "notes":
+                return <Notes />;
+
+            case "calendar":
+                return <Calendar />;
+
+            case "budget":
+                return <Budget />;
+
+            case "goals":
+                return <Goals />;
+
+            case "journal":
+                return <Journal />;
+
+            case "settings":
+                return <Settings />;
+
+            default:
+                return <Dashboard />;
+
+        }
+
+    }
 
     return (
 
@@ -20,7 +66,7 @@ export default function App() {
 
                 <Header />
 
-                <Dashboard />
+                {renderPage()}
 
             </div>
 
