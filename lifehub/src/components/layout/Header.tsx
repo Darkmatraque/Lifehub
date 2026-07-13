c6db18e74753c0d1ddb4de715f9b6e0942f5858c
Darkmatraque <kevin2007baumann@gmail.com>
2026-07-13 21:42:30 +0200

    Update Header.tsx

--- lifehub/src/components/layout/Header.tsx
+++ lifehub/src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import { useSettingsStore } from "../../store/settingsStore";
 export default function Header() {
 
     const username = useSettingsStore(
-        (state) => state.settings.username
+        state => state.settings.username
     );
 
     const now = new Date();
@@ -16,22 +16,17 @@ export default function Header() {
 
                 <h2>
 
-                    Bonjour {username} 👋
+                    Bonjour, {username} 👋
 
                 </h2>
 
                 <p>
 
                     {now.toLocaleDateString("fr-FR", {
-
                         weekday: "long",
-
                         day: "numeric",
-
                         month: "long",
-
                         year: "numeric"
-
                     })}
 
                 </p>
